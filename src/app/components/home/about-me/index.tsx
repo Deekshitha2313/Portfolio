3df2b4ad4c400adc434f22e4213120c32ab9433a
Deekshitha2313 <deekshi.g754@gmail.com>
2026-02-22 02:36:48 +0530

    Initial portfolio

---
+++ src/app/components/home/about-me/index.tsx
@@ -0,0 +1,88 @@
+import { getImgPath } from "@/utils/image";
+import Image from "next/image";
+
+const AboutMe = () => {
+  return (
+    <section>
+      <div className="relative bg-softGray py-10 md:py-32">
+        <div className="absolute top-0 w-full px-9">
+          <Image
+            src={getImgPath("/images/home/about-me/resume-bg-img.svg")}
+            alt="resume-bg-img"
+            width={1200}
+            height={348}
+            className="w-full"
+          />
+        </div>
+
+        <div className="relative z-10">
+          <div className="container">
+            <div className="flex items-center justify-between gap-2 border-b border-black pb-7">
+              <h2>About Me</h2>
+              <p className="text-xl text">( 01 )</p>
+            </div>
+
+            <div className="pt-10 xl:pt-16 flex gap-10 items-center justify-between">
+              <div className="w-[303px] h-[440px] hidden lg:flex">
+                <Image
+                  src={getImgPath("/images/home/about-me/pinkk.png")}
+                  alt="about-banner"
+                  width={303}
+                  height={440}
+                  className="w-full h-full"
+                />
+              </div>
+
+              <div className="w-full lg:max-w-2xl flex-1">
+                <p>
+                 Pre-Final-year engineering student specializing in Computer Science and Engineering, 
+                 with a strong foundation in Java. Seeking an entry-level position to apply technical knowledge,
+                  problem-solving skills, and continuous learning in a growth-oriented organization, 
+                  with a interest in frontend development and Artificial Intelligence.
+                </p>
+
+                <div className="grid grid-cols-3 py-10 xl:py-16 gap-5 border-b border-mistGray">
+                  {[
+            
+                    { count: "3", label: "Project Completed" },
+                  ].map((item, i) => (
+                    <div key={i}>
+                      <h3>{item.count}</h3>
+                      <p className="text-base md:text-lg text-black">
+                        {item.label}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+
+                <div className="pt-8 xl:pt-14 flex flex-col sm:flex-row items-center gap-4">
+                  <div className="flex items-center gap-3.5">
+                    <Image
+                      src={getImgPath("/images/icon/language.png")}
+                      alt="lang-icon"
+                      width={30}
+                      height={30}
+                    />
+                    <p className="text-base xl:text-xl text-black">Language</p>
+                  </div>
+                  <div className="flex flex-wrap justify-center items-center gap-2.5">
+                    {["English", "Tamil"].map((lang) => (
+                      <p
+                        key={lang}
+                        className="bg-white py-2 md:py-3.5 px-4 md:px-5 w-fit rounded-full text-base xl:text-xl"
+                      >
+                        {lang}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
---
+++ src/app/components/layout/header/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState } from "react";
+import Logo from "../logo";
+
+const Header = () => {
+    const handleDownloadPDF = () => {
+        window.print();
+    };
+    return (
+        <header className="navbar top-0 left-0 z-999 w-full absolute">
+            <div className="container">
+                <nav className="py-7">
+                    <div className="flex items-center gap-4 sm:gap-8">
+                        <div>
+                            <Logo />
+                        </div>
+
+                    
+                       <a
+    href="/my-resume.pdf"
+    download
+    className="relative overflow-hidden cursor-pointer w-fit py-2 sm:py-3 md:py-5 px-4 sm:px-5 md:px-7 border border-pink-500 rounded-full group hover:bg-pink-500"
+>
+    <span className="relative z-10 text-xl font-medium text-black group-hover:text-white transition-colors duration-300">
+        Download Resume
+    </span>
+</a>
+                         
+                        
+                    </div>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
